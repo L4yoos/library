3c4c502d7de3c7fe76d80b4e883bed5695f33a35
L4yoos <daleckikonrad1@gmail.com>
2025-07-15 20:04:48 +0200

    refactor for library.common

---
+++ common/src/main/java/com/library/common/exception/UserNotFoundException.java
@@ -0,0 +1,16 @@
+package com.library.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.UUID;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(UUID id) {
+        super("User with ID " + id + " not found.");
+    }
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
